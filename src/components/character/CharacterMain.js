78b70b9fa61c75effa5f9f2cf33718ce497a42b6
MadsJuelDev <92512864+MadsJuelDev@users.noreply.github.com>
2022-03-29 15:44:57 +0200

    Framer Motion Testing
    Working on Get hair Route animation
    Mads

--- src/components/character/CharacterMain.js
+++ src/components/character/CharacterMain.js
@@ -1,14 +1,30 @@
 import { React, useEffect, useState } from "react";
 import Hair from "./Hair";
+import axios from "axios";
+import { useQuery } from "react-query";
+import { motion } from "framer-motion";
 
 const CharacterMain = () => {
   //Here i can write javascript code and functions
+  const url = "/cc/api/hair/";
+  const [isOpen, setIsOpen] = useState(false);
+  const { isLoading, data } = useQuery("cc-hair", () => {
+    return axios.get(url);
+  });
+
+  if (isLoading) {
+    return <h2>Loading.. </h2>;
+  }
 
   return (
     <div className="content-section">
       <div className="content-section-title"></div>
+
       <div className="apps-card testimonial-grid">
-        <Hair />
+        <h2 className="grid-title flow">All hair assets</h2>
+        {data?.data.map((hair) => {
+          return <Hair hair={hair} />;
+        })}
       </div>
     </div>
   );
--- src/components/character/Hair.js
+++ src/components/character/Hair.js
@@ -1,10 +1,11 @@
-import { React } from "react";
+import { React, useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
+import { motion } from "framer-motion";
 
-const Hair = () => {
+const Hair = ({ hair }) => {
   const url = "/cc/api/hair/";
-
+  const [isOpen, setIsOpen] = useState(false);
   const { isLoading, data } = useQuery("cc-hair", () => {
     return axios.get(url);
   });
@@ -15,20 +16,25 @@ const Hair = () => {
 
   return (
     <>
-      <h2 className="grid-title testimonial">All hair assets</h2>
-      {data?.data.map((hair) => {
-        return (
-          <div className="app-card testimonial" key={hair}>
-            <span>{hair.id}</span>
+      <motion.div
+        layout
+        onClick={() => setIsOpen(!isOpen)}
+        className="app-card testimonial"
+        key={hair.id}
+        style={{ borderRadius: "14px" }}
+      >
+        <motion.h2 layout="position">{hair.name}</motion.h2>
+        {isOpen && (
+          <motion.div>
             <div className="app-card__subtext">The Color is: {hair.color}</div>
             <div className="app-card__subtext">The Style is: {hair.style}</div>
             <div className="app-card-buttons">
               <button className="content-button status-button">Update</button>
               <div className="menu"></div>
             </div>
-          </div>
-        );
-      })}
+          </motion.div>
+        )}
+      </motion.div>
     </>
   );
 };
